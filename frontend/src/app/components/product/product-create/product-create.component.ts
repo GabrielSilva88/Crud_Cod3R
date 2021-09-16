85f6b8466d04c71353f0babd9c96c6f3d41da783
GabrielSilva88 <gabrielmg.silva@outlook.com>
2021-09-16 13:06:23 -0300

    Correção processo

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -10,6 +10,10 @@ import { Router } from '@angular/router';
 })
 export class ProductCreateComponent implements OnInit {
 
+  product: Product ={
+    name:'',
+    price: 0
+  }
 
   constructor(private productService: ProductService,
      private router: Router) { }
@@ -18,7 +22,11 @@ export class ProductCreateComponent implements OnInit {
   }
 
   createProduct(): void{
-    this.productService.showMessage('Execução Com Sucessso')
+    this.productService.create(this.product).subscribe(()=>{
+    this.productService.showMessage('produto Criado')
+    this.router.navigate(['/products'])
+
+    })
 
   }
   cancel(): void{
